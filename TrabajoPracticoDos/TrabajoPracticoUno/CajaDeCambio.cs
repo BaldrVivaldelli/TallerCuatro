2132058e7f829e56bba368fe227d4214453afb26
Augusto Vivaldelli <aatv1@hotmail.com>
2017-09-14 21:59:35 -0300

    Add files via upload
    Modificaciones

--- TrabajoPracticoDos/TrabajoPracticoUno/CajaDeCambio.cs
+++ TrabajoPracticoDos/TrabajoPracticoUno/CajaDeCambio.cs
@@ -1,6 +1,12 @@
-﻿namespace TrabajoPracticoUno
+﻿using System;
+
+namespace TrabajoPracticoUno
 {
     internal class CajaDeCambio
     {
+        public static implicit operator CajaDeCambio(Rueda v)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- TrabajoPracticoDos/TrabajoPracticoUno/Motor.cs
+++ TrabajoPracticoDos/TrabajoPracticoUno/Motor.cs
@@ -1,8 +1,15 @@
-﻿namespace TrabajoPracticoUno
+﻿using System;
+
+namespace TrabajoPracticoUno
 {
     internal class Motor: IMovilidad
     {
         private string diametro { get; set; }
         private string cilindrada { get; set; }
+
+        public static implicit operator Motor(string v)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
